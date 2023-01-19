85d06d0e09e7bb1e02318217e389f1ffba04457f
Vinicius Ribeiro <vayne99@gmail.com>
2023-01-18 23:29:44 -0300

    setup mobile

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Habits from './components/Habits'
+
+function App() {
+
+  return (
+    <div>
+      <Habits />
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/Habits.tsx
@@ -0,0 +1,11 @@
+interface Props {
+
+}
+
+function Habits({  }: Props) {
+  return (
+    <div className="text-red-600">Habits</div>
+  )
+}
+
+export default Habits
