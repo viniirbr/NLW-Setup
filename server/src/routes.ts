5bf307dd63fa8662c5ec4916dc985475772132ef
Vinicius Ribeiro <vayne99@gmail.com>
2023-01-20 11:23:45 -0300

    create summary route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -116,4 +116,30 @@ export async function appRoutes(fastify: FastifyInstance) {
             }
         }
     })
+
+    fastify.get('/summary', async () => {
+        //SQLite
+        return await prisma.$queryRaw`
+        SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `
+    })
 }
